86e0c8f276c95352d1ed4f0e4a0a69709710d38f
Mateusz Sokola <mateuszsokola@gmail.com>
2015-09-14 01:20:58 +0200

    fix bugs and add tracking code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,6 +14,14 @@
     @yield('scripts')
 </head>
 <body class="@yield('page-name')">
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-61755879-1', 'auto');
+    ga('send', 'pageview');
+</script>
 <nav class="global-nav">
     <div class="container">
         <div class="global-nav-header">
@@ -114,8 +122,8 @@
             AT-1080 Vienna<br>
             Austria<br>
 
-            <a href="/imprint" class="imprint-link">
-                Imprint
+            <a href="mailto:office@hacking.healthcare" class="imprint-link">
+                Contact
             </a>
         </div>
     </div>
