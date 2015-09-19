33ffbe2964a9009a95e03b75d2a3b77d9ad7679b
Mateusz Sokola <mateuszsokola@gmail.com>
2015-09-19 23:24:17 +0200

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('title', '404 - hacking.healthcare')
+@section('page-name', 'imprint-page')
+
+@section('content')
+    <div class="global-content">
+        <div class="container">
+            <h1>Page does not exist</h1>
+
+            <p>
+                We updated our website last time. Some of resources are not existing anymore.
+            </p>
+
+            <p>
+                <a href="/">Go to the homepage</a>
+            </p>
+        </div>
+    </div>
+@endsection
